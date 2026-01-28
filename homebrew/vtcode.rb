bf53728060304a604a93eb0a60863c72db58e49b
Vinh Nguyen <vinhnguyen2308@gmail.com>
2026-01-29 00:28:31 +0700

    chore: update homebrew formula to v0.73.2

--- homebrew/vtcode.rb
+++ homebrew/vtcode.rb
@@ -2,15 +2,15 @@ class Vtcode < Formula
   desc "Rust-based terminal coding agent with semantic code intelligence"
   homepage "https://github.com/vinhnx/vtcode"
   license "MIT"
-  version "0.73.1"
+  version "0.73.2"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vinhnx/vtcode/releases/download/v#{version}/vtcode-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "466c98092191726b6d13ac05f7bc8283ae03bbd6436686aa571cac895d366760"
+      sha256 "a52efdb9c09d55df2bfc8f879c7565b8db7669934643b8bbf32ec747787ab502"
     else
       url "https://github.com/vinhnx/vtcode/releases/download/v#{version}/vtcode-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "7d1cdf415272215c1dd1fde6b3cd005a2a5a57342a2c7a9de58b6c7d6df47d2f"
+      sha256 "e602d8480379d4ff213271e9592732a2410df8e94cd1970316e6e746e618e976"
     end
   end
 
