9331b69d5043174fc10793f569349f60196bad4e
Vinh Nguyen <vinhnguyen2308@gmail.com>
2026-02-20 13:16:11 +0700

    feat: standardize MiniMax-M2.5 model identifier, promote it as the default, and update configuration defaults.

--- vtcode-config/src/constants/models/minimax.rs
+++ vtcode-config/src/constants/models/minimax.rs
@@ -1,4 +1,4 @@
 pub const DEFAULT_MODEL: &str = MINIMAX_M2_5;
 pub const SUPPORTED_MODELS: &[&str] = &[MINIMAX_M2_5, MINIMAX_M2];
 pub const MINIMAX_M2: &str = "MiniMax-M2";
-pub const MINIMAX_M2_5: &str = "MiniMaxAI/MiniMax-M2.5";
+pub const MINIMAX_M2_5: &str = "MiniMax-M2.5";
