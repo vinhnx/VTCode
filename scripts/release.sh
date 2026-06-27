6ec480e378b869564bc1a79623650c9908a9e6d5
Vinh Nguyen <vinhnguyen2308@gmail.com>
2026-06-27 09:25:37 +0700

    Revert "Fix release script"
    This reverts commit 947307b269f657ad4ccc3fed122b12254919c4a3.

--- scripts/release.sh
+++ scripts/release.sh
@@ -906,9 +906,7 @@ main() {
     if [[ "$release_argument" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         next_version="$release_argument"
     else
-        # Strip pre-release suffix (e.g. -dev, -rc1) before semver arithmetic
-        local base_version="${current_version%%-*}"
-        IFS='.' read -ra v <<< "$base_version"
+        IFS='.' read -ra v <<< "$current_version"
         case "$release_argument" in
             major)
                 local major_num=$((v[0] + 1))
